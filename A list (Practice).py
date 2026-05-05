935a76525ca6d328f428d308a64f7e552b5cbced
mimi73727170-coder <mimi73727170@gmail.com>
2026-05-05 17:35:22 +0530

    Add files via upload

---
+++ A list (Practice).py
@@ -0,0 +1,12 @@
+fruits = ["apple","banana","mango"]
+
+print(fruits)
+print(fruits[0])
+print(fruits[2])
+
+fruits.append("pineapple")
+
+print(len(fruits))
+
+for fruit in fruits :
+    print(fruit)
