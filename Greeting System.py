5eb94a6afc5ff8d3d5846ab58b1b3444a0f8a174
mimi73727170-coder <mimi73727170@gmail.com>
2026-05-05 16:50:31 +0530

    Add files via upload

---
+++ Greeting System.py
@@ -0,0 +1,8 @@
+name = input("Enter your name :")
+print("Hello"+" "+name)
+print("Your name has",len(name),"letters")
+print("Uppercase :",name.upper())
+if "a" in name :
+    print("Your name conntains'a'")
+else:
+    print("No'a' in your name")
